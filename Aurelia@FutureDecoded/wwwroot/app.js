c0f52cd7950dcebe537d4a2df8fb9907abd85a2f
GDiNardo <akelitz@hotmail.com>
2016-10-10 11:24:40 +0200

    Binding demo

---
+++ Aurelia@FutureDecoded/wwwroot/app.js
@@ -0,0 +1,16 @@
+﻿export class App {
+    message = 'Benvenuti a Future Decoded!';
+
+    constructor() {
+        this.name = '';
+        this.agenda = ['Binding', 'Composition', 'Dependency Injection', 'Routing & Navigation' ];
+    }
+
+    get tooltip() {
+        return 'sono Aurelia';
+    }
+
+    sayHello() {
+        alert(`Ciao ${this.name}!`);
+    }
+}
