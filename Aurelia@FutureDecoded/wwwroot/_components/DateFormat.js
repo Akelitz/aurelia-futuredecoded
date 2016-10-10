c32e97d66b44169d30762570c1de7926c9dc4c64
GDiNardo <akelitz@hotmail.com>
2016-10-10 12:03:38 +0200

    Converters & Behaviors demo

---
+++ Aurelia@FutureDecoded/wwwroot/_components/DateFormat.js
@@ -0,0 +1,9 @@
+﻿import moment from 'moment';
+
+export class DateFormatValueConverter {
+    toView(value, format) {
+        format = format || 'DD/MM/YYYY HH:mm';
+        return moment(value)
+            .format(format);
+    }
+}
--- Aurelia@FutureDecoded/wwwroot/tweets.js
+++ Aurelia@FutureDecoded/wwwroot/tweets.js
@@ -1,7 +1,14 @@
-﻿export class Tweets {
+﻿import moment from 'moment';
+
+export class Tweets {
     constructor() {
         this.tweets = [
                 { id: 1, title: 'Uno' },
                 { id: 2, title: 'Due' }
         ];
-    }}
+        this.now = '';
+        setInterval( _ => {
+            this.now = moment();
+        }, 200);
+    }
+}
